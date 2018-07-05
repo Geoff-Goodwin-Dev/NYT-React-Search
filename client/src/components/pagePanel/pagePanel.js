4a5aa772505552cc226b3fb3a8b746ef6a2f633d
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-07-05 16:17:48 -0400

    adds components for multiple page elements

---
+++ client/src/components/pagePanel/pagePanel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './pagePanel.css';
+
+const pagePanel = props => (
+  <div className="row">
+    <div className="col-sm-12">
+      <br />
+      <div className="card card-primary text-left">
+        <h3 className="card-header">Search Parameters</h3>
+        <div className="card-body">
+          <p>Test</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default pagePanel;
